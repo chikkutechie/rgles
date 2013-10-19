83b01ae87bb78bdefdce1ccd70033ea96bb72679
rtv <ranjith.tv@gmail.com>
2013-10-19 16:01:58 +0530

    texture example added

--- examples/texturing.cpp
+++ examples/texturing.cpp
@@ -36,7 +36,6 @@ class MyWindow: public rtv::RGLWin
 public:
     int mWidth;
     int mHeight;
-    bool mRotateCube;
     GLfloat mCubeRotationAngle;
     bool mInitialized;
     GLuint mTexture;
@@ -49,55 +48,38 @@ public:
     MyWindow() {
         mWidth = 256;
         mHeight = 256;
-        mRotateCube = true;
         mCubeRotationAngle = 10.0f;
         mInitialized = false;
     }
 
-    void createCube(float w, float h, float l) {
-        float x2 = w / 2.0f;
-        float y2 = h / 2.0f;
-        float z2 = l / 2.0f;
+    void createShapes() {
 
-        float v[] = { -x2, y2, z2, -x2, -y2, z2, x2, -y2, z2, x2, -y2, z2,
-                      x2, y2, z2, -x2, y2, z2, x2, y2, z2, x2, -y2, z2, x2, -y2, -z2, x2, -y2, -z2,
-                      x2, y2, -z2, x2, y2, z2, x2, y2, -z2, x2, -y2, -z2, -x2, -y2, -z2, -x2, -y2, -
-                      z2, -x2, y2, -z2, x2, y2, -z2, -x2, y2, -z2, -x2, -y2, -z2, -x2, -y2, z2, -x2,
-                      -y2, z2, -x2, y2, z2, -x2, y2, -z2, -x2, y2, -z2, -x2, y2, z2, x2, y2, z2, x2,
-                      y2, z2, x2, y2, -z2, -x2, y2, -z2, -x2, -y2, z2, -x2, -y2, -z2, x2, -y2, -z2,
-                      x2, -y2, -z2, x2, -y2, z2, -x2, -y2, z2
+        float v[] = {
+                    -0.5, -0.5,
+                    0.5, -0.5,
+                    0.5, 0.5,
+                    -0.5, 0.5
                     };
 
-        float n[] = {0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-                     1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-                     1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0,
-                     0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, -1.0, 0.0, 0.0,
-                     -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0,
-                     0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0,
-                     1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0, -1.0, 0.0, 0.0,
-                     -1.0, 0.0, 0.0, -1.0, 0.0
-                    };
-        float c[] = {0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
-                     1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0, 0.0, 0.0, 1.0,
-                     1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0,
-                     0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0,
-                     0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0,
-                     1.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0,
-                     1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, 0.0, 1.0,
-                     0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
-                     0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
-                     0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0,
-                     0.0, 1.0, 0.0, 1.0
+        float n[] = {
+        			0.0, 0.0, 1.0,
+        			0.0, 0.0, 1.0,
+        			0.0, 0.0, 1.0,
+        			0.0, 0.0, 1.0
                     };
 
-        float t[] = {1.0 - 1.0 / 4.0, 3.0 / 4.0, 1.0 - 1.0 / 4.0, 1.0 / 4.0, 1.0, 1.0 / 4.0, 1.0, 1.0 / 4.0, 1.0,
-                     3.0 / 4.0, 1.0 - 1.0 / 4.0, 3.0 / 4.0, 0.0, 3.0 / 4.0, 0.0, 1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0,
-                     1.0 / 4.0, 1.0 / 4.0, 1.0 / 4.0, 3.0 / 4.0, 0.0, 3.0 / 4.0, 1.0 / 4.0, 3.0 / 4.0, 1.0 / 4.0,
-                     1.0 / 4.0, 2.0 / 4.0, 1.0 / 4.0, 2.0 / 4.0, 1.0 / 4.0, 2.0 / 4.0, 3.0 / 4.0, 1.0 / 4.0,
-                     3.0 / 4.0, 2.0 / 4.0, 3.0 / 4.0, 2.0 / 4.0, 1.0 / 4.0, 3.0 / 4.0, 1.0 / 4.0, 3.0 / 4.0,
-                     1.0 / 4.0, 3.0 / 4.0, 3.0 / 4.0, 2.0 / 4.0, 3.0 / 4.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0,
-                     0.0, 0.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0,
-                     0.0, 1.0, 1.0
+        float c[] = {
+        		0.0, 0.0, 1.0, 1.0,
+        		0.0, 0.0, 1.0, 1.0,
+        		0.0, 0.0, 1.0, 1.0,
+        		0.0, 0.0, 1.0, 1.0
+        		};
+
+        float t[] = {
+        			0.0f, 0.0f,
+        			1.0f, 0.0f,
+        			1.0f, 1.0f,
+        			0.0f, 1.0f
                     };
 
         Vertices.clear();
@@ -134,7 +116,7 @@ public:
         		0xFF, 0x00, 0xFF, 0xFF
         };
 
-        createCube(1, 1, 1);
+        createShapes();
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 
@@ -170,10 +152,12 @@ public:
         glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mSpecular);
     }
 
-    void drawCube() {
-        glRotatef(mCubeRotationAngle, 1, 0, 0);
-        glRotatef(mCubeRotationAngle, 0, 1, 0);
+    void drawShapes() {
         glRotatef(mCubeRotationAngle, 0, 0, 1);
+        mCubeRotationAngle += 10;
+        if (mCubeRotationAngle > 360.0f) {
+            mCubeRotationAngle = 0.0f;
+        }
 
         glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
 
@@ -184,22 +168,16 @@ public:
 
         glColorPointer(4, GL_FLOAT, 0, &Colors[0]);
         glNormalPointer(GL_FLOAT, 0, &Normals[0]);
-        glVertexPointer(3, GL_FLOAT, 0, &Vertices[0]);
+        glVertexPointer(2, GL_FLOAT, 0, &Vertices[0]);
         glTexCoordPointer(2, GL_FLOAT, 0, &TexCoords[0]);
 
-        glDrawArrays(GL_TRIANGLES, 0, Vertices.size() / 3);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, Vertices.size() / 2);
 
         glDisableClientState(GL_VERTEX_ARRAY);
         glDisableClientState(GL_NORMAL_ARRAY);
         glDisableClientState(GL_COLOR_ARRAY);
         glDisableClientState(GL_TEXTURE_COORD_ARRAY);
 
-        if (mRotateCube) {
-            mCubeRotationAngle += 10.0f;
-            if (mCubeRotationAngle >= 360.0f) {
-                mCubeRotationAngle = 0.0f;
-            }
-        }
     }
 
     void draw() {
@@ -212,7 +190,7 @@ public:
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
 
-        drawCube();
+        drawShapes();
 
         GLenum error = glGetError();
         if (error != GL_NO_ERROR) {
